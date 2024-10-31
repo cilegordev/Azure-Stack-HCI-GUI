2ce31def5ebb326b61b521b6d80d23734b106ab9
unknown <123652808+cilegordev@users.noreply.github.com>
2024-11-01 01:27:05 +0700

    Components to enable GUI REDIST/Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.4_2.42007.9001.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.7_7.2208.15002.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.7_7.2208.15002.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.8_8.2208.12001.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.UI.Xaml.2.8_8.2208.12001.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx REDIST/Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x86__8wekyb3d8bbwe.Appx REDIST/Microsoft.VCLibs.140.00_14.0.30704.0_x86__8wekyb3d8bbwe.Appx REDIST/install.cmd

---
+++ REDIST/install.cmd
@@ -0,0 +1,15 @@
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.4_2.42007.9001.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.7_7.2208.15002.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.7_7.2208.15002.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.8_8.2208.12001.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.UI.Xaml.2.8_8.2208.12001.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x86__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
+powershell add-appxpackage D:\AZHCI_REDIST\Microsoft.VCLibs.140.00_14.0.30704.0_x86__8wekyb3d8bbwe.Appx
+shutdown /r /t 5
